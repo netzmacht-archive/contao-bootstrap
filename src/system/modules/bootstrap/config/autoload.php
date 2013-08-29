f0a239b145347e59feea1fdf70e1b8846f6236ab
David Molineus <support@marmalade.de>
2013-08-29 22:43:18 +0200

    mode theme based templates to a subdir so that the autoload creator can be used

--- src/system/modules/bootstrap/config/autoload.php
+++ src/system/modules/bootstrap/config/autoload.php
@@ -2,9 +2,9 @@
 
 /**
  * Contao Open Source CMS
- * 
- * Copyright (C) 2005-2013 Leo Feyer
- * 
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
  * @package Bootstrap
  * @link    https://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
@@ -25,35 +25,37 @@ ClassLoader::addNamespaces(array
  */
 ClassLoader::addClasses(array
 (
-	// Classes
-	'Netzmacht\Bootstrap\Icons'                    => 'system/modules/bootstrap/classes/Icons.php',
-	'Netzmacht\Bootstrap\Installer'                => 'system/modules/bootstrap/classes/Installer.php',
-	'Pagination'                                   => 'system/modules/bootstrap/classes/Pagination.php',
-
-	// DataContainer
-	'Netzmacht\Bootstrap\DataContainer\Bootstrap'  => 'system/modules/bootstrap/dataContainer/Bootstrap.php',
-	'Netzmacht\Bootstrap\DataContainer\Content'    => 'system/modules/bootstrap/dataContainer/Content.php',
-	'Netzmacht\Bootstrap\DataContainer\WrapperElements' => 'system/modules/bootstrap/dataContainer/WrapperElements.php',
+	// Forms
+	'Netzmacht\Bootstrap\BootstrapWidget'               => 'system/modules/bootstrap/forms/BootstrapWidget.php',
+	'Netzmacht\Bootstrap\FormButton'                    => 'system/modules/bootstrap/forms/FormButton.php',
 
 	// Modules
-	'Netzmacht\Bootstrap\BootstrapModule'          => 'system/modules/bootstrap/modules/BootstrapModule.php',
-	'Netzmacht\Bootstrap\ModuleNavbar'             => 'system/modules/bootstrap/modules/ModuleNavbar.php',
+	'Netzmacht\Bootstrap\BootstrapModule'               => 'system/modules/bootstrap/modules/BootstrapModule.php',
+	'Netzmacht\Bootstrap\ModuleNavbar'                  => 'system/modules/bootstrap/modules/ModuleNavbar.php',
 
 	// Elements
-	'Netzmacht\Bootstrap\BootstrapContentElement'  => 'system/modules/bootstrap/elements/BootstrapContentElement.php',
-	'Netzmacht\Bootstrap\BootstrapWrapperElement'  => 'system/modules/bootstrap/elements/BootstrapWrapperElement.php',
-	'Netzmacht\Bootstrap\ContentButton'            => 'system/modules/bootstrap/elements/ContentButton.php',
-	'Netzmacht\Bootstrap\ContentColumnSet'         => 'system/modules/bootstrap/elements/ContentColumnSet.php',
-	'Netzmacht\Bootstrap\ContentCarousel'            => 'system/modules/bootstrap/elements/ContentCarousel.php',
-	'Netzmacht\Bootstrap\ContentTab'               => 'system/modules/bootstrap/elements/ContentTab.php',
+	'Netzmacht\Bootstrap\ContentButton'                 => 'system/modules/bootstrap/elements/ContentButton.php',
+	'Netzmacht\Bootstrap\ContentAccordionGroup'         => 'system/modules/bootstrap/elements/ContentAccordionGroup.php',
+	'Netzmacht\Bootstrap\ContentCarousel'               => 'system/modules/bootstrap/elements/ContentCarousel.php',
+	'Netzmacht\Bootstrap\BootstrapWrapperElement'       => 'system/modules/bootstrap/elements/BootstrapWrapperElement.php',
+	'Netzmacht\Bootstrap\BootstrapContentElement'       => 'system/modules/bootstrap/elements/BootstrapContentElement.php',
+	'Netzmacht\Bootstrap\ContentColumnSet'              => 'system/modules/bootstrap/elements/ContentColumnSet.php',
+	'Netzmacht\Bootstrap\ContentTab'                    => 'system/modules/bootstrap/elements/ContentTab.php',
 
-	// Forms
-	'Netzmacht\Bootstrap\BootstrapWidget'          => 'system/modules/bootstrap/forms/BootstrapWidget.php',
-	'Netzmacht\Bootstrap\FormButton'               => 'system/modules/bootstrap/forms/FormButton.php',
+	// Classes
+	'Pagination'                                        => 'system/modules/bootstrap/classes/Pagination.php',
+	'Netzmacht\Bootstrap\Gravatar'                      => 'system/modules/bootstrap/classes/Gravatar.php',
+	'Netzmacht\Bootstrap\Icons'                         => 'system/modules/bootstrap/classes/Icons.php',
+	'Netzmacht\Bootstrap\Installer'                     => 'system/modules/bootstrap/classes/Installer.php',
+
+	// DataContainer
+	'Netzmacht\Bootstrap\DataContainer\WrapperElements' => 'system/modules/bootstrap/dataContainer/WrapperElements.php',
+	'Netzmacht\Bootstrap\DataContainer\Content'         => 'system/modules/bootstrap/dataContainer/Content.php',
+	'Netzmacht\Bootstrap\DataContainer\Bootstrap'       => 'system/modules/bootstrap/dataContainer/Bootstrap.php',
 
 	// Models
-	'Netzmacht\Bootstrap\ContentWrapperModel'      => 'system/modules/bootstrap/models/ContentWrapperModel.php',
-	'Netzmacht\Bootstrap\ContentWrapperCollection' => 'system/modules/bootstrap/models/ContentWrapperCollection.php',
+	'Netzmacht\Bootstrap\ContentWrapperCollection'      => 'system/modules/bootstrap/models/ContentWrapperCollection.php',
+	'Netzmacht\Bootstrap\ContentWrapperModel'           => 'system/modules/bootstrap/models/ContentWrapperModel.php',
 ));
 
 
@@ -62,18 +64,19 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-	'ce_bootstrap_button'    => 'system/modules/bootstrap/templates',
 	'ce_bootstrap_columnset' => 'system/modules/bootstrap/templates',
-	'ce_bootstrap_carousel'  => 'system/modules/bootstrap/templates',
-	'ce_bootstrap_tab'       => 'system/modules/bootstrap/templates',
-	'mod_navbar'             => 'system/modules/bootstrap/templates',
 	'mod_navbar_container'   => 'system/modules/bootstrap/templates',
-	'mod_search_advanced'    => 'system/modules/bootstrap/templates',
-	'nav_bootstrap_dropdown' => 'system/modules/bootstrap/templates',
-	'navbar'                 => 'system/modules/bootstrap/templates',
-	'navbar_brand'           => 'system/modules/bootstrap/templates',
-	'navbar_toggle'          => 'system/modules/bootstrap/templates',
+	'ce_bootstrap_tab'       => 'system/modules/bootstrap/templates',
+	'search_default'         => 'system/modules/bootstrap/templates',
+	'ce_bootstrap_carousel'  => 'system/modules/bootstrap/templates',
+	'mod_navbar'             => 'system/modules/bootstrap/templates',
 	'form_checkbox_generate' => 'system/modules/bootstrap/templates',
 	'form_radio_generate'    => 'system/modules/bootstrap/templates',
-	'pagination'             => 'system/modules/bootstrap/templates',
+	'navbar_brand'           => 'system/modules/bootstrap/templates',
+	'navbar'                 => 'system/modules/bootstrap/templates',
+	'com_bootstrap'          => 'system/modules/bootstrap/templates',
+	'ce_bootstrap_button'    => 'system/modules/bootstrap/templates',
+	'navbar_toggle'          => 'system/modules/bootstrap/templates',
+	'ce_accordion_group'     => 'system/modules/bootstrap/templates',
+	'nav_bootstrap_dropdown' => 'system/modules/bootstrap/templates',
 ));
