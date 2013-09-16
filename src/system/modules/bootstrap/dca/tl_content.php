9607dab229354ee6f121bb75ffed474ea5f07ce3
David Molineus <support@marmalade.de>
2013-09-16 09:57:44 +0200

    move dataAttribute to form config section

--- src/system/modules/bootstrap/dca/tl_content.php
+++ src/system/modules/bootstrap/dca/tl_content.php
@@ -317,7 +317,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['bootstrap_dataAttributes'] = array
 				'label'                   => &$GLOBALS['TL_LANG']['tl_content']['bootstrap_dataAttributes_name'],
 				'exclude'                 => true,
 				'inputType'               => 'customselect',
-				'options'                 => $GLOBALS['BOOTSTRAP']['button']['dataAttributes'],
+				'options'                 => $GLOBALS['BOOTSTRAP']['form']['dataAttributes'],
 				'reference'               => &$GLOBALS['TL_LANG']['tl_content']['bootstrap_buttons_types'],
 				'eval'                    => array('style' => 'width: 145px;', 'includeBlankOption' => true),
 			),
