4c7e7ac9231e53a0b9f725d007f33b7ca855c7ca
David Molineus <support@marmalade.de>
2013-09-16 09:47:02 +0200

    auto install dependencies on extension install

--- src/system/modules/bootstrap/classes/Installer.php
+++ src/system/modules/bootstrap/classes/Installer.php
@@ -16,6 +16,7 @@ class Installer extends \Backend
 	{
 		$this->import('BackendUser', 'User');
 		$this->import('Database');
+
 		parent::__construct();
 
 		$this->User->authenticate();
@@ -25,6 +26,85 @@ class Installer extends \Backend
 	}
 
 	public function runOnce()
+	{
+		$this->setupSections();
+		$this->installDependencies();
+	}
+
+	protected function installDependencies()
+	{
+		require_once TL_ROOT . '/system/modules/bootstrap/config/bootstrap/installer.php';
+
+		$GLOBALS['TL_CONFIG']['bootstrapInstalledDependencies'] = deserialize($GLOBALS['TL_CONFIG']['bootstrapInstalledDependencies'], true);
+
+		$files = \Files::getInstance();
+
+		foreach($GLOBALS['BOOSTRAP']['installer'] as $dependencyName => $dependency)
+		{
+			// already installed
+			if(in_array($dependencyName, $GLOBALS['TL_CONFIG']['bootstrapInstalledDependencies']))
+			{
+				continue;
+			}
+
+			// condition failed
+			if(isset($dependency['condition']) && !call_user_func($dependency['condition']))
+			{
+				continue;
+			}
+
+			$content = file_get_contents($dependency['file']);
+
+			$tempName = tempnam(TL_ROOT . '/system/tmp', 'bootstrap_');
+
+			$tempFile = fopen($tempName, 'w+');
+			fwrite($tempFile, $content);
+			fclose($tempFile);
+
+			$zip = new \ZipReader(str_replace(TL_ROOT, '', $tempName));
+
+			while($zip->next())
+			{
+				// does not match root
+				if($dependency['root'] != '' && strpos($zip->file_name, $dependency['root']) !== 0)
+				{
+					continue;
+				}
+
+				$fileName = str_replace($dependency['root'], '', $zip->file_name);
+
+				// not in the included paths
+				if(isset($dependency['paths']) && !$this->fileMatchPaths($fileName, $dependency['paths']))
+				{
+					continue;
+				}
+
+				var_dump($dependency['target'] . $fileName);
+
+				$file = new \File($dependency['target'] . $fileName);
+				$file->write($zip->unzip());
+				$file->close();
+			}
+
+
+
+			$GLOBALS['TL_CONFIG']['bootstrapInstalledDependencies'][] = $dependencyName;
+
+			$this->log(
+				sprintf('Bootstrap dependency "%s" installed', $dependencyName),
+				'Netzmacht\Bootstrap\Installer::installDependencies',
+				TL_INFO
+			);
+		}
+
+		$files->delete($tempName);
+
+		$config = \Config::getInstance();
+		$config->add('$GLOBALS[\'TL_CONFIG\'][\'bootstrapInstalledDependencies\']', serialize($GLOBALS['TL_CONFIG']['bootstrapInstalledDependencies']));
+		$config->save();
+	}
+
+	protected function setupSections()
 	{
 		if(!$this->User->isAdmin) {
 			return;
@@ -42,4 +122,16 @@ class Installer extends \Backend
 		$config->save();
 	}
 
+	protected function fileMatchPaths($fileName, $paths)
+	{
+		foreach($paths as $path)
+		{
+			if(strpos($fileName, $path) === 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
---
+++ src/system/modules/bootstrap/config/bootstrap/installer.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * @package   netzmacht-columnset
+ * @author    David Molineus <http://www.netzmacht.de>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2012 David Molineus netzmacht creative
+ *
+ **/
+
+$GLOBALS['BOOSTRAP']['installer'] = array
+(
+	'hooky' => array
+	(
+		'file'      => 'https://github.com/xat/contao-hooky/archive/master.zip',
+		'root'      => 'contao-hooky-master/src/',
+		'target'    => '',
+		'condition' => function() {
+			return !class_exists('Hooky');
+		},
+	),
+
+	'bootstrap' => array
+	(
+		'file'      => 'https://github.com/twbs/bootstrap/archive/v3.0.0.zip',
+		'root'      => 'bootstrap-3.0.0/',
+		'target'    => 'assets/bootstrap/bootstrap/',
+		'paths'     => array
+		(
+			'js',
+			'less',
+			'fonts',
+			'dist',
+			'LICENSE',
+			'assets/js/html5shiv.js',
+			'assets/js/respond.min.js',
+
+		),
+	),
+
+	'bootstrap-select' => array
+	(
+		'file'      => 'https://github.com/silviomoreto/bootstrap-select/archive/1.3.1.zip',
+		'root'      => 'bootstrap-select-1.3.1',
+		'target'    => 'assets/bootstrap/bootstrap-select/',
+	),
+
+	'jquery-touchSwipe' => array
+	(
+		'file'      => 'https://github.com/mattbryson/TouchSwipe-Jquery-Plugin/archive/1.6.4.zip',
+		'root'      => 'TouchSwipe-Jquery-Plugin-1.6.4/',
+		'target'    => 'assets/bootstrap/jquery-touchSwipe/',
+		'paths'     => array
+		(
+			'jquery.touchSwipe.min.js',
+			'README.md'
+		),
+	)
+);
