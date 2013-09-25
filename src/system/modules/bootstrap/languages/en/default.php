d5d46ea3ef51a701f1c6973ceee5f2b6f2c97e8b
djoesch <daniel@joesch.biz>
2013-09-25 17:42:04 +0200

    Create default.php

---
+++ src/system/modules/bootstrap/languages/en/default.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * content elements
+ */
+$GLOBALS['TL_LANG']['CTE']['bootstrap'] = 'Bootstrap';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabs'] = 'Tab elements';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carousel'] = 'Carousel elements';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_columnset'][0] = 'Article based Column-Set';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_columnset'][1] = 'Allows to create any desired Column-Sets as a content element.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][0] = 'Button';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][1] = 'Allocates a button as a content element.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][0] = 'Combined buttons ';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][1] = 'Element for combined buttons like toolbars/groups/dropdowns.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselStart'][0] = 'Carousel Start';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselStart'][1] = 'Starts a bootstrap carousel.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselPart'][0] = 'Carousel part-element';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselPart'][1] = 'Part-element separates parts of the Bootstrap Carousel.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselEnd'][0] = 'Carousel end-element';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_carouselEnd'][1] = 'End-element of the Bootstrap Carousel.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabStart'][0] = 'Tab Start';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabStart'][1] = 'Starts a Tab-element.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabPart'][0] = 'Tab part-element';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabPart'][1] = 'Part-element to separate parts of a Bootstrap Carousel';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabEnd'][0] = 'Tab end';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_tabEnd'][1] = 'End-element of the Bootstrap Tab';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart'][0] = 'Accordion Group start';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart'][1] = 'Groups all accordion elements and forcing merely one element to be opened.';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd'][0] = 'Accordion group end';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd'][1] = 'End of an accordion group';
+
+
+
+
+/**
+ * form elements
+ */
+$GLOBALS['TL_LANG']['FFL']['button'][0] = 'Button';
+$GLOBALS['TL_LANG']['FFL']['button'][1] = 'Button with text, icon or an image.';
+
+
+/**
+ *
+ */
+$GLOBALS['TL_LANG']['MSC']['bootstrap'][0] = 'Bootstrap Config';
+$GLOBALS['TL_LANG']['MSC']['bootstrap'][1] = 'Change settings of the Bootstrap-Extension.';
+
+$GLOBALS['TL_LANG']['MSC']['bootstrapToggleNavigation'] = 'Show/hide navigation';
+
+$GLOBALS['TL_LANG']['MSC']['bootstrap_carousel'] = 'Bootstrap Carousel: %s';
+$GLOBALS['TL_LANG']['MSC']['bootstrap_tab'] = 'Bootstrap Tab: %s';
+
+/**
+ * errors
+ */
+$GLOBALS['TL_LANG']['ERR']['wrapperStartNotExists'] = 'The related start-element of "%s" could not be found. Please set it up first.';
