dca17c2c69d4b6220b14cea666e5d9e995405e43
David Molineus <mail@netzmacht.de>
2014-02-28 10:28:50 +0100

    add log information to runonce installer

--- runonce/runonce.php
+++ runonce/runonce.php
@@ -11,17 +11,6 @@
  * @copyright 2013 netzmacht creative David Molineus
  */
 
-/*
-if(!defined('TL_MODE')) {
-	define('TL_MODE', 'BE');
-}
-
-require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/system/initialize.php';
-*/
-
-/**
- * Initialize the system
- */
 
 $controller = new Netzmacht\Bootstrap\Installer();
 $controller->run();
