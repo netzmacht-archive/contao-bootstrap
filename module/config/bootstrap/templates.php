18e006f975423ae1e7b40f4cfecdc91d55118432
David Molineus <mail@netzmacht.de>
2014-03-05 14:01:35 +0100

    provide custom template for radio elements

--- module/config/bootstrap/templates.php
+++ module/config/bootstrap/templates.php
@@ -26,6 +26,7 @@ $GLOBALS['BOOTSTRAP']['templates'] = array
 			'ce_accordion_start',
 			'form',
 			'formhelper_element_checkboxes',
+			'formhelper_element_radios',
 			'mod_breadcrumb',
 			'mod_comment_form',
 			'mod_search_advanced',
