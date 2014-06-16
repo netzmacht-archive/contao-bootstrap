79620c547d71f23b87aa848748f898e4bf9aeb5d
David Molineus <mail@netzmacht.de>
2014-06-16 10:16:32 +0200

    support input groups for html5 elements

--- module/config/bootstrap/form.php
+++ module/config/bootstrap/form.php
@@ -65,6 +65,27 @@ $GLOBALS['BOOTSTRAP']['form'] = array
 			'allowInputGroup'  => true,
 		),
 
+		'email' => array
+		(
+			'allowInputGroup'  => true,
+		),
+
+
+		'digit' => array
+		(
+			'allowInputGroup'  => true,
+		),
+
+		'tel' => array
+		(
+			'allowInputGroup'  => true,
+		),
+
+		'url' => array
+		(
+			'allowInputGroup'  => true,
+		),
+
 		'textarea' => array
 		(
 			'allowInputGroup'  => true,
